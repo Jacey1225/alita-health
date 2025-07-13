3fe33eee3db6775a8a2d4957a535fdad83b20774
Aahana Jain <aahanajain03@gmail.com>
2025-07-13 01:28:37 -0400

    finished settings + chat pgs

---
+++ src/chat/chat.js
@@ -0,0 +1,13 @@
+const text = "type here...";
+const speed = 100; // milliseconds per character
+let i = 0;
+
+function typeWriter() {
+  if (i < text.length) {
+    document.getElementById("typewriter").textContent += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+typeWriter();
